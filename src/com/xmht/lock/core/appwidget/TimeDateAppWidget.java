7b6b12860e2a4297dea8099d959d5236a8d2cc9e
yushaojian <yushaojian13@163.com>
2014-11-25 15:02:57 +0800

    tmp

--- src/com/xmht/lock/core/appwidget/TimeDateAppWidget.java
+++ src/com/xmht/lock/core/appwidget/TimeDateAppWidget.java
@@ -3,58 +3,29 @@ package com.xmht.lock.core.appwidget;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.text.format.Time;
-import android.widget.RemoteViews;
 
-import com.xmht.lockair.R;
+import com.xmht.lock.core.service.AppWidgetService;
 
 public class TimeDateAppWidget extends AppWidgetProvider {
-    private static final String ACTION_UPDATE = "com.xmht.lock.air.tick";
-    
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        String action = intent.getAction();
-        if (ACTION_UPDATE.equals(action)) {
-            update(context);
-        }
-    }
-    
-    private void update(Context context) {
-        Time time = new Time();
-        time.setToNow();
-        
-        ComponentName provider = new ComponentName(
-                context.getApplicationContext(), TimeDateAppWidget.class);
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_time_date_1);
-        remoteViews.setTextViewText(R.id.date, time.month + " " + time.monthDay);
-        remoteViews.setTextViewText(R.id.week, time.weekDay + " ");
-        remoteViews.setTextViewText(R.id.time, time.hour + ":" + time.minute + ":" + time.second);
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-        
-        appWidgetManager.updateAppWidget(provider, remoteViews);
-    }
 
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-        Intent intent = new Intent(ACTION_UPDATE);
-        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        Intent intent = new Intent(context, AppWidgetService.class);
+        PendingIntent alarmIntent = PendingIntent.getService(context, 0, intent, 0);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
-                1000, alarmIntent);
+        alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(),
+                1000 * 60, alarmIntent);
     }
 
     @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
-        Intent intent = new Intent(ACTION_UPDATE);
-        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        Intent intent = new Intent(context, AppWidgetService.class);
+        PendingIntent alarmIntent = PendingIntent.getService(context, 0, intent, 0);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(alarmIntent);
     }
--- src/com/xmht/lock/core/service/AppWidgetService.java
+++ src/com/xmht/lock/core/service/AppWidgetService.java
@@ -1,78 +1,173 @@
 
 package com.xmht.lock.core.service;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.IBinder;
-import android.text.Html;
-import android.text.SpannableStringBuilder;
+import android.text.format.Time;
 import android.widget.RemoteViews;
 
-import com.xmht.lock.core.appwidget.TimeDateAppWidget2;
-import com.xmht.lock.core.data.time.engine.TickEngine;
-import com.xmht.lock.core.data.time.format.TimeFormatter;
-import com.xmht.lock.core.data.time.observe.SecondObserver;
-import com.xmht.lock.debug.LOG;
-import com.xmht.lock.utils.XMTagHandler;
+import com.xmht.lock.core.appwidget.TimeDateAppWidget;
+import com.xmht.lock.utils.Utils;
 import com.xmht.lockair.R;
 
-public class AppWidgetService extends Service implements SecondObserver {
-    private TickEngine tickEngine;
-
+public class AppWidgetService extends Service {
+    
     @Override
     public void onCreate() {
         super.onCreate();
-        startTick();
-        LOG.v("");
+        Time time = new Time();
+        time.setToNow();
+        String text = time.hour + ":" + time.minute;
+        String font = "fonts/Helvetica-Light.ttf";
+        Bitmap bitmap = createBitmap(text, font);
+        showBitmap(bitmap);
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        stopTick();
-        LOG.v("");
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        onUpdate();
+        return START_STICKY;
     }
 
+    private Bitmap curBitmap;
+
+    public void onUpdate() {
+        new AsyncTask<Void, Bitmap, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                // 1. 0:0:59s load curr time bitmap form file
+                // 2. 0:1:0s publishProgress(values); remoteView refer bitmap
+                // 3. gen next bitmap, save to file
+                // 4. currBitmap = next Bitmap
+
+                Time time = new Time();
+                long now = System.currentTimeMillis();
+                time.set(now);
+
+                if (time.second == 59) {
+                    loadCurBitmap();
+                } else if (time.second == 0) {
+                    if (curBitmap != null) {
+                        publishProgress(curBitmap);
+                        curBitmap = null;
+                    } else {
+                        String text = time.hour + ":" + time.minute;
+                        String font = "fonts/Helvetica-Light.ttf";
+                        Bitmap bitmap = createBitmap(text, font);
+                        publishProgress(bitmap);
+                    }
+
+                    time.set(now + 1000 * 60);
+                    String text = time.hour + ":" + time.minute;
+                    String font = "fonts/Helvetica-Light.ttf";
+                    Bitmap bitmap = createBitmap(text, font);
+                    saveNextBitmap(bitmap);
+                }
+                
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Bitmap... values) {
+                if (values == null || values.length == 0) {
+                    return;
+                }
+
+                showBitmap(values[0]);
+            }
+        }.execute();
+
+    }
+    
+    private Bitmap createBitmap(String text, String font) {
+        Typeface typeface = Utils.createTypeface(AppWidgetService.this, font);
+
+        Paint paint = new Paint();
+        paint.setTypeface(typeface);
+        paint.setAntiAlias(true);
+        paint.setTextAlign(Align.LEFT);
+        paint.setTextSize(80);
+        paint.setColor(Color.WHITE);
+
+        Rect bound = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bound);
+        Bitmap bitmap = Bitmap.createBitmap(bound.width(), bound.height(),
+                Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(0x6600ff00);
+        canvas.drawText(text, -bound.left, -bound.top, paint);
+        return bitmap;
+    }
+
+    private void saveNextBitmap(Bitmap bitmap) {
+        OutputStream os = null;
+        try {
+            os = openFileOutput("widget", MODE_PRIVATE);
+            bitmap.compress(CompressFormat.PNG, 100, os);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void loadCurBitmap() {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput("widget");
+            curBitmap = BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            curBitmap = null;
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    private void showBitmap(Bitmap bitmap) {
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.app_widget);
+        remoteViews.setImageViewBitmap(R.id.iv_time, bitmap);
+        AppWidgetManager appWidgetManager = AppWidgetManager
+                .getInstance(AppWidgetService.this);
+        ComponentName provider = new ComponentName(getApplicationContext(),
+                TimeDateAppWidget.class);
+        appWidgetManager.updateAppWidget(provider, remoteViews);
+    };
+    
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
-
-    private void startTick() {
-        LOG.v("");
-        if (tickEngine != null) {
-            return;
-        }
-
-        tickEngine = TickEngine.getInstance();
-        tickEngine.registerObservers(this);
-    }
-
-    private void stopTick() {
-        LOG.v("");
-        if (tickEngine == null) {
-            return;
-        }
-
-        tickEngine.removeObservers(this);
-    }
-
-    @Override
-    public void onUpdate() {
-        
-        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.widget_time_date_3);
-        SpannableStringBuilder builder =  new SpannableStringBuilder(Html.fromHtml("<body>123213====<ft-1>12312312312</ft-1></body>", null, new XMTagHandler(this)));
-        remoteViews.setTextViewText(R.id.date, TimeFormatter.getDate(false, false, " "));
-        remoteViews.setTextViewText(R.id.week, TimeFormatter.getWeek(false, false));
-//        remoteViews.setTextViewText(R.id.time, TimeFormatter.getTime(false, true, ":"));
-        remoteViews.setTextViewText(R.id.time, builder);
-        remoteViews.setTextViewText(R.id.am_pm, TimeFormatter.getAM(true));
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-
-        ComponentName provider = new ComponentName(getApplicationContext(), TimeDateAppWidget2.class);
-        appWidgetManager.updateAppWidget(provider, remoteViews);
-    }
-
 }
